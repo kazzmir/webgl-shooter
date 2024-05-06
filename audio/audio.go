14e8085e54545adc20b7fbdbb6db03c90cba2cc1
Jon Rafkind <jon@rafkind.com>
2024-05-06 11:13:12 -0700

    use ogg instead of mp3

--- audio/audio.go
+++ audio/audio.go
@@ -4,18 +4,20 @@ import (
     _ "embed"
     "fmt"
     "bytes"
+    "io"
 
-    "github.com/hajimehoshi/ebiten/v2/audio/mp3"
+    // "github.com/hajimehoshi/ebiten/v2/audio/mp3"
+    "github.com/hajimehoshi/ebiten/v2/audio/vorbis"
     // libAudio "github.com/hajimehoshi/ebiten/v2/audio"
 )
 
-//go:embed effects/hit1.mp3
+//go:embed effects/hit1.ogg
 var Hit1Data []byte
 
-//go:embed effects/shoot1.mp3
+//go:embed effects/shoot1.ogg
 var Shoot1Data []byte
 
-//go:embed music/stellar-pulse.mp3
+//go:embed music/stellar-pulse.ogg
 var SongStellarPulseData []byte
 
 type AudioName string
@@ -26,11 +28,11 @@ const AudioStellarPulseSong = AudioName("stellar-pulse")
 
 var AllSounds []AudioName = []AudioName{AudioHit1, AudioShoot1, AudioStellarPulseSong}
 
-func LoadSound(name AudioName, sampleRate int) (*mp3.Stream, error) {
+func LoadSound(name AudioName, sampleRate int) (io.Reader, error) {
     switch name {
-        case AudioHit1: return mp3.DecodeWithSampleRate(sampleRate, bytes.NewReader(Hit1Data))
-        case AudioShoot1: return mp3.DecodeWithSampleRate(sampleRate, bytes.NewReader(Shoot1Data))
-        case AudioStellarPulseSong: return mp3.DecodeWithSampleRate(sampleRate, bytes.NewReader(SongStellarPulseData))
+        case AudioHit1: return vorbis.DecodeWithSampleRate(sampleRate, bytes.NewReader(Hit1Data))
+        case AudioShoot1: return vorbis.DecodeWithSampleRate(sampleRate, bytes.NewReader(Shoot1Data))
+        case AudioStellarPulseSong: return vorbis.DecodeWithSampleRate(sampleRate, bytes.NewReader(SongStellarPulseData))
     }
 
     return nil, fmt.Errorf("No such audio effect %v", name)
